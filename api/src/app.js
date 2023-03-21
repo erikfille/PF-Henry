14c33b53a2fb0cbb95a70c938dbbd7facc78f829
SantiArguello <Saanti_arguello@hotmail.com>
2023-03-21 15:05:12 -0300

    Creacion de archivo Servidor

---
+++ api/src/app.js
@@ -0,0 +1,18 @@
+const hapi = require("@hapi/hapi")
+require("./database.js")
+
+const init = async () => {
+    const server = new hapi.server({
+        port: 3000,
+        host:"localHost",
+    })
+
+
+
+
+
+ await server.start();
+  console.log(`el servidor esta corriendo en ${server.info.uri}`);
+}
+
+init()
