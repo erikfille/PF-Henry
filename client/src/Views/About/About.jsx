0f9edc4c4cf800251e744936c95501aa012c9338
Noelia Lombardo <noedenisel@MacBook-Pro-de-Noe-Denise.local>
2023-03-22 20:21:58 -0300

    Creando el aboutus

--- client/src/Views/About/About.jsx
+++ client/src/Views/About/About.jsx
@@ -4,9 +4,81 @@ import styles from "./About.module.css"
 
 export default function About() {
   return (
-    <div className='containerAbout'>
+    <div className='containerAbout d-flex flex-wrap justify-content-center align-items-center align-content-center align-content-between'>
       <h1>¡El equipo detras de este hermoso proyecto!</h1>   
       
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
+      <div class="card  card-spacing" style={{width: "18rem"}} >
+        <img src="..." class="card-img-top" alt="foto-persona"/>
+          <div class="card-body">
+            <p class="card-text">Persona</p>
+          </div>
+        <img src="..." class="imgPais" alt="bandera-pais"/>
+      </div>
+
 </div>  
    
   )
