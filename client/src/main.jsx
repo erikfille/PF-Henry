5e4ff9d6916c0ae11245bada2cf4e602fa9631e9
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-22 17:24:11 -0300

    Import bootstrap css & js, limpieza css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+// Import css bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
