ca28ddee3e1637f56be3b315f39889c76ceb2016
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-23 15:45:56 -0300

    Add columns responsive

--- client/src/Views/Contacto/Contacto.jsx
+++ client/src/Views/Contacto/Contacto.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import styles from "./Contacto.module.css"
+import BreadCrump from "../../components/BreadCrump/BreadCrump";
+import Meta from "../../components/Meta/Meta";
+import styles from "./Contacto.module.css";
 
 export default function Contacto() {
-  return (
-    <div>Contacto</div>
-  )
+	return (
+		<div>
+			<Meta title={"Contacto"} />
+			<BreadCrump title='Contacto' />
+			<div class='container-xl text-center'>
+				<div class='row align-items-center'>
+					<div class='col-xs-12 col-sm-6'>One of three columns</div>
+					<div class='col-xs-12 col-sm-6'>One of three columns</div>
+				</div>
+			</div>
+		</div>
+	);
 }
