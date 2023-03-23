e81bc8be80b489f635c624d63b2ae9fba252d9e0
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-23 14:43:50 -0300

    Eliminado importacion bootstrap en main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// Import css bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-// Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
