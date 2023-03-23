a25ed01f1ecfd02208adcc529a0151cbb6c1551b
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-23 15:03:01 -0300

    Agregado iconos, fix bg blanco

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { BiMenu } from "react-icons/bi";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { MdOutlineShoppingCart } from "react-icons/md";
 // styles
 import styles from "./Header.module.css";
 
 const Header = () => {
 	return (
-		<nav className='navbar navbar-expand-lg bg-body-tertiary navbar-light bg-light'>
-			<div className='container-fluid bg-light'>
-				<div className={`d-flex align-items-center ${styles.logoContainer}`}>
+		<nav className='navbar navbar-expand-lg bg-body-tertiary'>
+			<div className='container-fluid'>
+				<div className={`d-flex align-items-center ms-3 ${styles.logoContainer}`}>
 					<h3>PetsAmerica</h3>
 					<img src={Logo} alt='Logo' className={`align-text-top ms-2 ${styles.logo}`} />
 				</div>
@@ -23,25 +25,33 @@ const Header = () => {
 					className={styles.menuButton}
 				/>
 				<div className='collapse navbar-collapse justify-content-end me-4 ' id='navbarNav'>
-					<div className='navbar-nav'>
-						<NavLink to='/inicio' className='nav-item me-4'>
+					<div className='navbar-nav align-items-center'>
+						<NavLink to='/inicio' className='nav-item me-3'>
 							Inicio
 						</NavLink>
-						<NavLink to='/about' className='nav-item me-4'>
+						<NavLink to='/about' className='nav-item me-3'>
 							Sobre Nosotros
 						</NavLink>
-						<NavLink to='/tienda' className='nav-item me-4'>
+						<NavLink to='/tienda' className='nav-item me-3'>
 							Tienda
 						</NavLink>
-						<NavLink to='/servicios' className='nav-item me-4'>
+						<NavLink to='/servicios' className='nav-item me-3'>
 							Servicios
 						</NavLink>
-						<NavLink to='/contacto' className='nav-item me-4'>
+						<NavLink to='/contacto' className='nav-item me-3'>
 							Contacto
 						</NavLink>
-						<NavLink to='/faqs' className='nav-item me-4'>
+						<NavLink to='/faqs' className='nav-item me-3'>
 							Faqs
 						</NavLink>
+						<div className={`d-flex align-items-center ${styles.buttonContainer}`}>
+							<NavLink to='/user' className='nav-item me-3' id='userButton'>
+								<HiOutlineUserCircle />
+							</NavLink>
+							<NavLink to='/cart' className='nav-item me-3' id='cartButton'>
+								<MdOutlineShoppingCart />
+							</NavLink>
+						</div>
 					</div>
 				</div>
 			</div>
