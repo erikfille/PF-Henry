812f594264f1fed4129bb89bac4ff9f2278d8623
Petri36 <petriniagustin41521@gmail.com>
2023-03-23 20:15:00 -0300

    modificacion y correccion de errores en el modelo de producto

--- api/src/models/productos_servicios/Producto_servicio.js
+++ api/src/models/productos_servicios/Producto_servicio.js
@@ -1,47 +1,46 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 const productoServicioSchema = new Schema({
-    titulo: {
-        type: String,
-        required:true
-    },
-    esProducto: {
-        type: Boolean,
-        required:true
-    },
-    esServicio:{
-        type: Boolean,
-        required:true
-    },
+  titulo: {
+    type: String,
+    required: true,
+  },
+  esProducto: {
+    type: Boolean,
+    required: true,
+  },
+  esServicio: {
+    type: Boolean,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  imagen: {
+    type: Buffer,
+    contentType: String,
+  },
+  rating: {
+    type: Number,
+  },
+  stock: {
+    type: Number,
+  },
+  activo: {
+    type: Boolean,
+  },
+  categoria: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Categoria",
+      },
+    ],
+  },
+  fechaDeCreacion: {
+    type: Date,
+  },
+});
 
-    precio: {
-        type: Number,
-        required:true
-    },
-    imagen: {
-        type: Buffer,
-        contentType: String
-    },
-    rating: {
-        type: Number,
-    },
-    stock: {
-        type: Number,
-        
-    },
-    activo: {
-        type:Boolean
-    },
-    categoria: {
-        type: [{
-            type: Schema.Types.ObjectId,
-            ref:"Categoria"
-        }]
-    },
-    fechaDeCreacion: {
-        type:Date
-    }
-
-})
-
-module.exports = model("Producto_servicio", productoServicioSchema)
+module.exports = model("Producto_servicio", productoServicioSchema);
