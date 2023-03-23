bb9ac22c03ad006b3a9476a3961ae5695dd7b087
SantiArguello <Saanti_arguello@hotmail.com>
2023-03-23 18:19:06 -0300

    creacion y correcion de archivos para rutas

--- api/src/app.js
+++ api/src/app.js
@@ -1,25 +1,30 @@
 const hapi = require("@hapi/hapi");
 require("./database.js");
-const usuariosRoutes = require("./routes/usuarios.js")
-const rolRoutes = require("./routes/roles")
-//const productosRoutes = require("./routes/productos")
+const usuariosRoutes = require("./routes/usuarios.js");
+const rolRoutes = require("./routes/roles");
+//const createProductosRoutes = require("./routes/createProducto")
 //const categoriasRoutes = require("./routes/categorias")
+//const productoDetailRoutes = require("./routes/productoDetail.js")
+//const allProductosRoutes = require("./routes/allProductos.js")
+//const stockProductosRoutes = require("./routes/stockProductos.js")
+
 
 const init = async () => {
-    const server = new hapi.Server({
-        port: 3000,
-        host:"localHost",
-    })
+  const server = new hapi.Server({
+    port: 3000,
+    host: "localHost",
+  });
 
-    server.route(usuariosRoutes)
-    server.route(rolRoutes)
-// server.route(productosRoutes)
-// server.route(categoriasRoutes)
+  server.route(usuariosRoutes);
+  server.route(rolRoutes);
+  // server.route(createProductosRoutes)
+  // server.route(categoriasRoutes)
+  // server.route(productoDetailRoutes)
+  // server.route(allProductosRoutes)
+  // server.route(stockProductosRoutes)
 
-
-
- await server.start();
+  await server.start();
   console.log(`el servidor esta corriendo en ${server.info.uri}`);
-}
+};
 
-init()
+init();
---
+++ api/src/routes/allProductos.js
@@ -0,0 +1,9 @@
+// descomentar todo y no cambiar el nombre el de la variable; en notion estan que rutas hay que hacer para cada modelo
+
+
+// const allProductosRoutes = [{
+    
+
+
+// }]
+// module.exports= allProductosRoutes
