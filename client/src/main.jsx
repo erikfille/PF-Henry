5d7f4396e9b86573e31249f334226083d9360f06
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-23 09:56:25 -0300

    Import al final del css, evita ser pisado

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 // Import css bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
