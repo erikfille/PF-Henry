37eefa10749b1175e37e3326af6b2a2f3a23f129
SantiArguello <Saanti_arguello@hotmail.com>
2023-03-23 17:42:05 -0300

    avance ene rutas usuario

--- api/src/routes/usuarios.js
+++ api/src/routes/usuarios.js
@@ -7,16 +7,13 @@ const usuariosRoutes = [
     path: "/users",
     options: {
       async handler(request, h) {
-        const { name, surname, email, rol, password, address } =
-          request.payload;
+        const { name, surname, email, rol, password, address } = request.payload;
         try {
           // Buscar el rol por su nombre
-          const rolEncontrado = await Rol.findOne({ rol })
+          const rolEncontrado = await Rol.findOne({ nombre: rol });
           // Si el rol no existe, devolver un error
           if (!rolEncontrado) {
-            return h
-              .response({ error: "El rol especificado no existe" })
-              .code(400);
+            return h.response({ error: "El rol especificado no existe" }).code(400);
           }
           // Crear un nuevo usuario con el rol encontrado
           const nuevoUsuario = new Usuario({
@@ -35,7 +32,7 @@ const usuariosRoutes = [
         }
       },
     },
-    },
+  },
  {
     method: "GET",
     path: "/users/{email}",
@@ -45,7 +42,12 @@ const usuariosRoutes = [
 
         try {
           // Buscar el usuario por su id y hacer el populate del campo "rol"
-          const usuario = await Usuario.findOne({email}).populate("rol");
+          const usuario = await Usuario.findOne({ email }).populate("rol");
+          
+          // Si el usuario es proveedor, agregar la propiedad "proveedor_id"
+      if (usuario.rol.nombre === "proveedor") {
+        usuario.proveedor_id = usuario._id;
+      }
 
           return h.response(usuario);
         } catch (error) {
