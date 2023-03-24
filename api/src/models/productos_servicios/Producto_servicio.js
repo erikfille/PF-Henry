f13524fad10793c92512fc03e990c2607f6cdab2
SantiArguello <Saanti_arguello@hotmail.com>
2023-03-24 14:28:36 -0300

    correcion de la ruta Crear produtos

--- api/src/models/productos_servicios/Producto_servicio.js
+++ api/src/models/productos_servicios/Producto_servicio.js
@@ -1,46 +1,54 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 const productoServicioSchema = new Schema({
-    titulo: {
-        type: String,
-        required:true
+  titulo: {
+    type: String,
+    required: true,
+  },
+  tipo: {
+    type: String,
+    enum: ['Producto', 'Servicio'],
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  imagen: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  stock: {
+    type: Number,
+    validate: {
+      validator: function(v) {
+        return v >= 0;
+      },
+      message: props => `${props.value} debe ser un número positivo.`
     },
-    tipo: {
-        type: String,
-        enum: ['Producto', 'Servicio'],
-        required: true
-    },
-    precio: {
-        type: Number,
-        required:true
-    },
-    imagen: {
-        type: Buffer,
-        contentType: String
-    },
-    rating: {
-        type: Number,
-    },
-    stock: {
-        type: Number,
-        
-    },
-    activo: {
-        type:Boolean
-    },
-    categoria: {
-        type: [{
-            type: Schema.Types.ObjectId,
-            ref:"Categoria"
-        }]
-    },
-    proveedor: {
-        ref:"Proveedor",
-        type: Schema.Types.ObjectId
-    },
-    fechaDeCreacion: {
-        type:Date
-    }
-})
+  },
+  activo: {
+    type: Boolean,
+    default: true,
+  },
+  categoria: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: "Categoria",
+    }],
+  },
+  proveedor: {
+    type: Schema.Types.ObjectId,
+    ref: "Proveedor",
+  },
+  fechaDeCreacion: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = model("Producto_servicio", productoServicioSchema)
+module.exports = model("ProductoServicio", productoServicioSchema);
