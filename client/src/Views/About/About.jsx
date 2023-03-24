fe250540825be037a35d8516b88f5dc5b89514e1
Noelia Lombardo <noedenisel@MacBook-Pro-de-Noe-Denise.local>
2023-03-24 12:54:47 -0300

    Style: add the rendering of the img of the country according to the country of each member

--- client/src/Views/About/About.jsx
+++ client/src/Views/About/About.jsx
@@ -1,82 +1,53 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+
 import styles from "./About.module.css"
+
+function Integrante({ nombre, pais }) {
+  const [bandera, setBandera] = useState('');
+
+  useEffect(() => {
+    const fetchBandera = async () => {
+      const res = await fetch(`https://restcountries.com/v2/name/${pais}`);
+      const data = await res.json();
+      if (data.length > 0) {
+        setBandera(data[0].flag);
+      }
+    };
+    fetchBandera();
+  }, [pais]);
+
+  return (
+    <Col xs={6} sm={3} className="d-flex justify-content-center align-items-center">
+    <div className="text-center">
+      <img src="" alt="foto persona" className="rounded-circle mb-2"></img>
+      <p>{nombre}</p>
+      <img src={bandera} alt="imagen bandera" className={styles.imgBandera}></img>
+      <span className="text-center d-none">{pais}</span>
+    </div>
+  </Col>
+);
+}
 
 export default function About() {
   return (
     <Container>
       <h2 style={{ textAlign: 'center' }}>¡El equipo detrás de este hermoso proyecto!</h2>
 
-      <Row>
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
+      <Row >
+        <Integrante nombre="Noelia" pais="Argentina" />
+        <Integrante nombre="Jhon Tatto" pais="Colombia" />
+        <Integrante nombre="Jonha" pais="Venezuela" />
+        <Integrante nombre="Santi" pais="Argentina" />
       </Row>
 
-      <Row>
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
-        <Col xs={6} sm={3}>
-          <div className="d-flex flex-column align-items-center">
-            <img src="" alt="foto persona" className="rounded-circle"></img>
-            <p>Nombre integrante</p>
-            <img src="" alt="imagen bandera"></img>
-          </div>
-        </Col>
-
+      <Row >
+        <Integrante nombre="Integrante 5" pais="Chile" />
+        <Integrante nombre="Integrante 6" pais="México" />
+        <Integrante nombre="Integrante 7" pais="Perú" />
+        <Integrante nombre="Integrante 8" pais="Uruguay" />
       </Row>
-
     </Container>
-  )
+  );
 }
 
