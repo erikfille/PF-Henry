b37af55cae7fa827981f4b85c9ddd65de77544fd
Noelia Lombardo <noedenisel@MacBook-Pro-de-Noe-Denise.local>
2023-03-24 10:53:17 -0300

    Feat: Make the structure of about us

--- client/src/Views/About/About.jsx
+++ client/src/Views/About/About.jsx
@@ -1,12 +1,82 @@
-import React from 'react'
-
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import styles from "./About.module.css"
 
 export default function About() {
   return (
-    <div>
-      Estoy en About
+    <Container>
+      <h2 style={{ textAlign: 'center' }}>¡El equipo detrás de este hermoso proyecto!</h2>
 
-    </div>
+      <Row>
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+        <Col xs={6} sm={3}>
+          <div className="d-flex flex-column align-items-center">
+            <img src="" alt="foto persona" className="rounded-circle"></img>
+            <p>Nombre integrante</p>
+            <img src="" alt="imagen bandera"></img>
+          </div>
+        </Col>
+
+      </Row>
+
+    </Container>
   )
 }
+
