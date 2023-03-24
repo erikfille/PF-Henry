c818d47ab2c7d80612326d8c1198ec4942d2b74b
Blesial <chonaquirelli@gmail.com>
2023-03-24 11:21:37 -0300

    Modifique algunos modelos

--- api/src/routes/categorias.js
+++ api/src/routes/categorias.js
@@ -1,7 +1,6 @@
 const Categoria = require('../models/categorias/Categoria')
+const productos_servicios = require('../models/productos_servicios/Producto_servicio')
 
-
-// tengo que modificar el modelo de categorias para dsp poder hacer los populates! 
 
 const categoriasRoutes = [
     {
@@ -9,10 +8,9 @@ const categoriasRoutes = [
     path: '/allCategories',
     handler: async (request, h) => {
         try {
-            const categoriasProductos = await Categoria.find({ categoria: ['Productos'] })
-            //.populate(productos_servicios);
-            const categoriasServicios = await Categoria.find({ categoria: ['Servicios'] })
-            //.populate(productos_servicios);
+            const categoriasProductos = await Categoria.find({ categoria: ['Productos'] }).populate(productos_servicios);
+            const categoriasServicios = await Categoria.find({ categoria: ['Servicios'] }).populate(productos_servicios);
+
             const AllCategorias = categoriasProductos.concat(categoriasServicios);
     
            return h.response(AllCategorias);
