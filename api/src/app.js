f3dcb6c7b602bdd0b1631c567d5be28fb7d5e6cf
jhon <jhondcorrearuiz@gmail.com>
2023-03-24 11:44:15 -0500

    ruta mascotas ok

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ const allProductosRoutes = require("./routes/allProductos.js")
 const stockProductosRoutes = require("./routes/stockProductos.js")
 //const postProvedorRoutes = require("./routes/postProveedor.js")
 //const getProveedorRoutes = require("./routes/getProveedor.js")
-//const createMascotaRoutes = require("./routes/createMascota.js")
+const createMascotaRoutes = require("./routes/createMascota.js")
 
 const init = async () => {
   const server = new hapi.Server({
@@ -27,7 +27,7 @@ const init = async () => {
   server.route(stockProductosRoutes)
 //server.route(postProvedorRoutes)
 //server.route(getProveedorRoutes)
-//server.route(createMascotaRoutes)
+ server.route(createMascotaRoutes)
 
   
   await server.start();
