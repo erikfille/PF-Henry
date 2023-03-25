12135befd665f93434fcc1e761e44977f4342c3d
Petri36 <petriniagustin41521@gmail.com>
2023-03-25 08:04:04 -0300

    descomentadas las líneas que involucran al GetProveedor

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ const productoDetailRoutes = require("./routes/productoDetail.js");
 const allProductosRoutes = require("./routes/allProductos.js");
 const stockProductosRoutes = require("./routes/stockProductos.js");
 const postProvedorRoutes = require("./routes/postProveedor.js");
-//const getProveedorRoutes = require("./routes/getProveedor.js")
+const getProveedorRoutes = require("./routes/getProveedor.js")
 const createMascotaRoutes = require("./routes/createMascota.js");
 const ordenDecompraRoutes = require("./routes/ordenDeCompra.js");
 
@@ -34,9 +34,9 @@ const init = async () => {
   server.route(allProductosRoutes);
   server.route(stockProductosRoutes);
   server.route(postProvedorRoutes);
-  //server.route(getProveedorRoutes)
+  server.route(getProveedorRoutes)
   server.route(createMascotaRoutes);
-  //server.route(ordenDecompraRoutes)
+  server.route(ordenDecompraRoutes)
 
   await server.start();
   console.log(`el servidor esta corriendo en ${server.info.uri}`);
