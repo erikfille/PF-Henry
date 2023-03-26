023fdd625d2c87bbb0114db2cad87b20abee939b
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-26 16:57:08 -0300

    Delete logo from widget, create module.css

--- client/src/components/LoginWidget/LoginWidget.jsx
+++ client/src/components/LoginWidget/LoginWidget.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import styles from "./LoginWidget.module.css";
+
 // Google Auth
 import { gapi } from "gapi-script";
 import GoogleLogin from "react-google-login";
@@ -8,187 +10,177 @@ import GoogleLogin from "react-google-login";
 // Validations
 import validation from "./validation";
 
-const logoImage = "../../../public/images/logo-pet.png";
-
 export default function LoginWidget(props) {
-  const { childProps } = props;
+	const { childProps } = props;
 
-  // Google Auth Data
-  const [user, setUser] = useState({});
+	// Google Auth Data
+	const [user, setUser] = useState({});
 
-  const [userData, setUserData] = useState({
-    name: "",
-    surname: "",
-    email: "",
-    password: "",
-    rol: "",
-  });
+	const [userData, setUserData] = useState({
+		name: "",
+		surname: "",
+		email: "",
+		password: "",
+		rol: "",
+	});
 
-  // Own Auth Data
-  const [errors, setErrors] = useState({
-    user: "",
-    password: "",
-  });
+	// Own Auth Data
+	const [errors, setErrors] = useState({
+		user: "",
+		password: "",
+	});
 
-  // Google Auth Logic
-  const clientID =
-    "756465634743-0hd8ke48er3tkrt4siag4o30m7h73a8c.apps.googleusercontent.com";
+	// Google Auth Logic
+	const clientID = "756465634743-0hd8ke48er3tkrt4siag4o30m7h73a8c.apps.googleusercontent.com";
 
-  useEffect(() => {
-    const start = () => {
-      gapi.auth2.init({
-        clientId: clientID,
-      });
-    };
-    gapi.load("client:auth2", start);
-  }, []);
+	useEffect(() => {
+		const start = () => {
+			gapi.auth2.init({
+				clientId: clientID,
+			});
+		};
+		gapi.load("client:auth2", start);
+	}, []);
 
-  const onSuccess = async (response) => {
-    console.log(response);
-    let user = {
-      user: response.profileObj.email,
-      name: response.profileObj.givenName,
-      surname: response.profileObj.familyName,
-      id: response.profileObj.googleId,
-      image: response.profileObj.imageUrl,
-    };
+	const onSuccess = async (response) => {
+		console.log(response);
+		let user = {
+			user: response.profileObj.email,
+			name: response.profileObj.givenName,
+			surname: response.profileObj.familyName,
+			id: response.profileObj.googleId,
+			image: response.profileObj.imageUrl,
+		};
 
-    try {
-      const response = await axios.post(`/users`, user);
-    } catch (err) {
-      const response = await axios.post(`/users/login`, user);
-    }
+		try {
+			const response = await axios.post(`/users`, user);
+		} catch (err) {
+			const response = await axios.post(`/users/login`, user);
+		}
 
-    /*
+		/*
     - Busca al usuario en la base de datos por el mail
         - Si existe el mail, trae la info, crea el token y lo sube al localStorage
         - Si no existe el mail, crea el usuario con la info de Google y lo pasa a otra pagina donde:
             - Se hace un get del usuario creado y se le da a elegir si quiere comprar o quiere vender, para asignarle el rol de usuario.
             - Se agrega la info de rol al estado, se hace un put agregandole el nuevo rol de usuario y se setea el nuevo token, pusheandolo al localStorage.
     */
-  };
+	};
 
-  const onFailure = () => {
-    console.log("something went wrong");
-  };
+	const onFailure = () => {
+		console.log("something went wrong");
+	};
 
-  // Own Auth Logic
-  function handleInputChange(e) {
-    setUserData({
-      ...userData,
-      [e.target.name]: e.target.value,
-    });
+	// Own Auth Logic
+	function handleInputChange(e) {
+		setUserData({
+			...userData,
+			[e.target.name]: e.target.value,
+		});
 
-    setErrors(
-      validation({
-        ...userData,
-        [e.target.name]: e.target.value,
-      })
-    );
-  }
+		setErrors(
+			validation({
+				...userData,
+				[e.target.name]: e.target.value,
+			})
+		);
+	}
 
-  function handleSubmit(e) {
-    e.preventDefault();
+	function handleSubmit(e) {
+		e.preventDefault();
 
-    /*
+		/*
     - Guardado de la info
     - Trae la info del usuario
     - Crea el Token y lo envía al LocalStorage
     */
-  }
+	}
 
-  return (
-    <div className="widgetContainer">
-      <div className="loginLogo">
-        <img src={logoImage} alt="logoApp" />
-      </div>
-      <h1 className="loginTitle">¡Bienvenido a Pet App!</h1>
-      <hr />
-      <p>Ingrese sus datos para acceder</p>
-      <div className="loginFormContainer">
-        <form onSubmit={handleSubmit}>
-          {childProps.type === "signup" && (
-            <>
-              <div>
-                <input
-                  placeholder="Nombre"
-                  id="name"
-                  name="name"
-                  value={userData.user}
-                  onChange={handleInputChange}
-                  className={errors.name && "danger"}
-                  type="text"
-                ></input>
-                {errors.name && <p>{errors.name}</p>}
-              </div>
-              <div>
-                <input
-                  placeholder="Apellido"
-                  id="surname"
-                  surname="surname"
-                  value={userData.user}
-                  onChange={handleInputChange}
-                  classsurname={errors.surname && "danger"}
-                  type="text"
-                ></input>
-                {errors.surname && <p>{errors.surname}</p>}
-              </div>
-            </>
-          )}
+	return (
+		<div className='col-12 col-md-6 d-flex flex-column align-items-center'>
+			<h1 className=''>¡Bienvenido a Pet App!</h1>
+			<hr />
+			<p>Ingrese sus datos para acceder</p>
+			<div className=''>
+				<form onSubmit={handleSubmit}>
+					{childProps.type === "signup" && (
+						<>
+							<div>
+								<input
+									placeholder='Nombre'
+									id='name'
+									name='name'
+									value={userData.user}
+									onChange={handleInputChange}
+									className={errors.name && "danger"}
+									type='text'></input>
+								{errors.name && <p>{errors.name}</p>}
+							</div>
+							<div>
+								<input
+									placeholder='Apellido'
+									id='surname'
+									surname='surname'
+									value={userData.user}
+									onChange={handleInputChange}
+									classsurname={errors.surname && "danger"}
+									type='text'></input>
+								{errors.surname && <p>{errors.surname}</p>}
+							</div>
+						</>
+					)}
 
-          <div>
-            <input
-              placeholder="Email"
-              id="mail"
-              name="mail"
-              value={userData.user}
-              onChange={handleInputChange}
-              className={errors.mail && "danger"}
-              type="text"
-            ></input>
-            {errors.mail && <p>{errors.mail}</p>}
-          </div>
-          <div>
-            <input
-              placeholder="Contraseña"
-              id="password"
-              name="password"
-              value={userData.password}
-              onChange={handleInputChange}
-              className={errors.password && "danger"}
-              type="password"
-            ></input>
-            {errors.password && <p>{errors.password}</p>}
-          </div>
-          {childProps.type === "signup" && (
-            <div>
-              <select name="rol" id="rol">
-                <option value="" disabled selected>
-                  ¿Qué quieres hacer?
-                </option>
-                <option value="customer">Quiero Comprar</option>
-                <option value="proveedor">Quiero Vender</option>
-              </select>
-            </div>
-          )}
-          <hr />
-          <button className="loginButton">{childProps.button}</button>
-        </form>
-      </div>
-      <div className="loginGoogleAuth">
-        <GoogleLogin
-          clientId={clientID}
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={"single_host_policy"}
-        />
-      </div>
-      <div className="loginOrSignup">
-        <p>{childProps.message}</p>
-        <a href={childProps.anchorPath}>
-          <span>{childProps.accountAnchor}</span>
-        </a>
-      </div>
-    </div>
-  );
+					<div>
+						<input
+							placeholder='Email'
+							id='mail'
+							name='mail'
+							value={userData.user}
+							onChange={handleInputChange}
+							className={errors.mail && "danger"}
+							type='text'></input>
+						{errors.mail && <p>{errors.mail}</p>}
+					</div>
+					<div>
+						<input
+							placeholder='Contraseña'
+							id='password'
+							name='password'
+							value={userData.password}
+							onChange={handleInputChange}
+							className={errors.password && "danger"}
+							type='password'></input>
+						{errors.password && <p>{errors.password}</p>}
+					</div>
+					{childProps.type === "signup" && (
+						<div>
+							<select name='rol' id='rol'>
+								<option value='' disabled selected>
+									¿Qué quieres hacer?
+								</option>
+								<option value='customer'>Quiero Comprar</option>
+								<option value='proveedor'>Quiero Vender</option>
+							</select>
+						</div>
+					)}
+					<hr />
+					<button className={`button ${styles.button}`}>{childProps.button}</button>
+				</form>
+			</div>
+			<div className=''>
+				<GoogleLogin
+					clientId={clientID}
+					onSuccess={onSuccess}
+					onFailure={onFailure}
+					cookiePolicy={"single_host_policy"}
+				/>
+			</div>
+			<div className=''>
+				<p>{childProps.message}</p>
+				<a href={childProps.anchorPath}>
+					<span>{childProps.accountAnchor}</span>
+				</a>
+			</div>
+		</div>
+	);
 }
