e9d7256eeb6fd3bd6ec95ec8763ce85d7a6c1f87
SantiArguello <Saanti_arguello@hotmail.com>
2023-03-26 19:04:30 -0300

    pequeñas correciones de productos

--- api/src/routes/Productos/allProductos.js
+++ api/src/routes/Productos/allProductos.js
@@ -7,7 +7,9 @@ const allProductosRoutes = [
         path: '/allProductos',
         handler: async (request, h) => {
             try {
-               const productosActivos = await allProductos.find({activo: true});
+                const productosActivos = await allProductos.find({ activo: true })
+                .populate('categoria')
+                .populate('proveedor',"nombre");
                return h.response(productosActivos);
             } catch (err) {
                 return h.response(err).code(500);
