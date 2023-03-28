9bea4689f434af8ad728032b4baa5ec647c220f7
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-28 16:02:45 -0300

    Agregando store services, cambiando renderizado

--- client/src/components/ServiceContainer/ServiceContainer.jsx
+++ client/src/components/ServiceContainer/ServiceContainer.jsx
@@ -1,22 +1,27 @@
-import { services } from '../../Views/Servicios/helperService';
-import ServiceCard from '../ServiceCard/ServiceCard';
+import { services } from "../../Views/Servicios/helperService";
+import ServiceCard from "../ServiceCard/ServiceCard";
 
-const ServiceContainer = () => {
-    return (
-        <>
-            {
-            services.map(service => {
-                return <ServiceCard
-                    title = {service.title}
-                    service = {service.service}
-                    country = {service.country}
-                    address = {service.address}
-                    image = {service.image}
-                />
-            })
-            }
-        </>
-    )
-}
+const ServiceContainer = ({ services }) => {
+	return (
+		<>
+			{services.map((service) => {
+				return (
+					<ServiceCard
+						nombre={service.nombre}
+						tipo={service.tipo}
+						pais={service.pais}
+						direccion={service.direccion}
+						image={service.image}
+						mail={service.mail}
+						telefono={service.telefono}
+						descripcion={service.descripcion}
+						rating={service.rating}
+						horarioAtencion={service.horarioAtencion}
+					/>
+				);
+			})}
+		</>
+	);
+};
 
 export default ServiceContainer;
--- client/src/hooks/useStore.js
+++ client/src/hooks/useStore.js
@@ -1,97 +1,139 @@
 import { create } from "zustand";
 import axios from "axios";
 
-export const useProduct = create((set, get) => ({
-  allProducts: [],
-  filteredProducts: [],
-  filteredProductsWOSearch: [],
-  categories: [],
-  cartState: false,
-  cartProducts: [],
-  getProducts: async () => {
-    try {
-      let response = await axios.get("/allProductos");
-      let products = response.data;
-      set((state) => ({ allProducts: products }));
-      set((state) => ({ filteredProducts: products }));
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  getCategories: async () => {
-    try {
-      let response = await axios.get("/categorias");
-      let categorias = response.data.categorias;
-      console.log("getCategories: ", categorias);
-      set((state) => ({ categories: categorias }));
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  setFilter: (products) => {
-    set((state) => ({ filteredProducts: products }));
-    set((state) => ({ filteredProductsWOSearch: products }));
-  },
-  ordered: (order) => {
-    if (order === "alfabetico-ascendente") {
-      set((state) => ({
-        filteredProducts: state.filteredProducts.sort((a, b) => {
-          if (a.titulo > b.titulo) return 1;
-          if (a.titulo < b.titulo) return -1;
-          return 0;
-        }),
-      }));
-    }
-    if (order === "alfabetico-descendente") {
-      set((state) => ({
-        filteredProducts: state.filteredProducts.sort((a, b) => {
-          if (a.titulo < b.titulo) return 1;
-          if (a.titulo > b.titulo) return -1;
-          return 0;
-        }),
-      }));
-    }
-    if (order === "popularidad") {
-      set((state) => ({
-        filteredProducts: state.filteredProducts.sort((a, b) => {
-          if (a.rating < b.rating) return 1;
-          if (a.rating > b.rating) return -1;
-          return 0;
-        }),
-      }));
-    }
-  },
-  searchProduct: (productos) => {
-    if (typeof productos === "object" && productos.length) {
-      set((state) => ({ filteredProducts: productos }));
-    }
-  },
-  setCartAdd: (productId, quantity) => {
-    let product = axios
-      .get(`/product-detail/${productId}`)
-      .catch((error) => window.alert("Algo salio mal, intentalo nuevamente"));
+// IMPORT PROVISIONAL
+import { services } from "../Views/Servicios/helperService";
 
-    product.quantity = quantity;
-    set((state) => ({ cartProducts: [...state.cartProducts, cartProduct] }));
-  },
-  setCartRemove: (productId) => {
-    set((state) => ({
-      cartProducts: state.cartProducts.filter((p) => p._id !== productId),
-    }));
-  },
-  setActiveCart: () => {
-    const { cartState } = get();
-    set((state) => ({ cartState: state.cartState ? false : true }));
-    console.log(cartState);
-  },
+export const useProduct = create((set, get) => ({
+	allProducts: [],
+	filteredProducts: [],
+	filteredProductsWOSearch: [],
+	categories: [],
+	cartState: false,
+	cartProducts: [],
+	getProducts: async () => {
+		try {
+			let response = await axios.get("/allProductos");
+			let products = response.data;
+			set((state) => ({ allProducts: products }));
+			set((state) => ({ filteredProducts: products }));
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	getCategories: async () => {
+		try {
+			let response = await axios.get("/categorias");
+			let categorias = response.data.categorias;
+			console.log("getCategories: ", categorias);
+			set((state) => ({ categories: categorias }));
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	setFilter: (products) => {
+		set((state) => ({ filteredProducts: products }));
+		set((state) => ({ filteredProductsWOSearch: products }));
+	},
+	ordered: (order) => {
+		if (order === "alfabetico-ascendente") {
+			set((state) => ({
+				filteredProducts: state.filteredProducts.sort((a, b) => {
+					if (a.titulo > b.titulo) return 1;
+					if (a.titulo < b.titulo) return -1;
+					return 0;
+				}),
+			}));
+		}
+		if (order === "alfabetico-descendente") {
+			set((state) => ({
+				filteredProducts: state.filteredProducts.sort((a, b) => {
+					if (a.titulo < b.titulo) return 1;
+					if (a.titulo > b.titulo) return -1;
+					return 0;
+				}),
+			}));
+		}
+		if (order === "popularidad") {
+			set((state) => ({
+				filteredProducts: state.filteredProducts.sort((a, b) => {
+					if (a.rating < b.rating) return 1;
+					if (a.rating > b.rating) return -1;
+					return 0;
+				}),
+			}));
+		}
+	},
+	searchProduct: (productos) => {
+		if (typeof productos === "object" && productos.length) {
+			set((state) => ({ filteredProducts: productos }));
+		}
+	},
+	setCartAdd: (productId, quantity) => {
+		let product = axios
+			.get(`/product-detail/${productId}`)
+			.catch((error) => window.alert("Algo salio mal, intentalo nuevamente"));
+
+		product.quantity = quantity;
+		set((state) => ({ cartProducts: [...state.cartProducts, cartProduct] }));
+	},
+	setCartRemove: (productId) => {
+		set((state) => ({
+			cartProducts: state.cartProducts.filter((p) => p._id !== productId),
+		}));
+	},
+	setActiveCart: () => {
+		const { cartState } = get();
+		set((state) => ({ cartState: state.cartState ? false : true }));
+		console.log(cartState);
+	},
 }));
 
 const useUser = create((set) => ({
-  user: {},
-  getUserInfo: async () => {
-    set((state) => ({ user: state.bears + 1 }));
-  },
-  setUserInfo: async (userData) => {
-    let post = await axios.post("/user", userData);
-  },
+	user: {},
+	getUserInfo: async () => {
+		set((state) => ({ user: state.bears + 1 }));
+	},
+	setUserInfo: async (userData) => {
+		let post = await axios.post("/user", userData);
+	},
+}));
+
+export const useServices = create((set, get) => ({
+	// allServices: [],
+	// filteredServices: [],
+	allServices: services,
+	filteredServices: services,
+
+	filteredServicesWOSearch: [],
+	// getServices: async () => {
+	// 	try {
+	// 		let response = await axios.get("/proveedores");
+	// 		let services = response.data;
+	// 		set((state) => ({ allServices: services }));
+	// 		set((state) => ({ filteredServices: services }));
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 	}
+	// },
+	ordered: (order) => {
+		if (order === "alfabetico-ascendente") {
+			set((state) => ({
+				filteredServices: state.filteredServices.sort((a, b) => {
+					if (a.nombre > b.nombre) return 1;
+					if (a.nombre < b.nombre) return -1;
+					return 0;
+				}),
+			}));
+		}
+		if (order === "alfabetico-descendente") {
+			set((state) => ({
+				filteredServices: state.filteredServices.sort((a, b) => {
+					if (a.nombre < b.nombre) return 1;
+					if (a.nombre > b.nombre) return -1;
+					return 0;
+				}),
+			}));
+		}
+	},
 }));
