dbdd36227b5e310788b223b3d3791512119331d1
erikfille <109306990+erikfille@users.noreply.github.com>
2023-03-29 20:35:22 -0300

    Loader Implementado, Paginado armado
    Loader implementado en Tienda y en ProductDetail
    Paginado en Tienda

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -5,34 +5,44 @@ import BreadCrump from "../../components/BreadCrump/BreadCrump";
 import ProductDetail from "../../components/ProductDetail/ProductDetail";
 import ProductReviews from "../../components/ProductReviews/ProductReviews";
 import ProviderDetail from "../../components/ProviderDetail/ProviderDetail";
+import Loader from "../../components/Loader/Loader";
 import axios from "axios";
 
 import styles from "./Detail.module.css";
 
 export default function Detail() {
-	const [productDetail, setProductDetail] = useState({});
-	const [providerDetail, setProviderDetail] = useState({});
+  const [productDetail, setProductDetail] = useState({});
+  const [providerDetail, setProviderDetail] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const { productId } = useParams();
 
   useEffect(() => {
-    axios
+    setLoading(true);
+    let response = axios
       .get(`/product-detail/${productId}`)
       .then((data) => {
         setProductDetail(data.data);
       })
-      .catch((error) => window.alert("Algo salio mal, intentalo nuevamente"));
+      .catch((error) => window.alert("Algo salio mal, intentalo nuevamente"))
+      .finally(() => setLoading(false));
 
-		return () => setProductDetail({});
-	}, [productId]);
+    return () => setProductDetail({});
+  }, [productId]);
 
   return (
-    <div className='home-wrapper-2 '>
+    <div className="home-wrapper-2 ">
       <Meta title={"Detalle"} />
       <BreadCrump title="Detalle de Producto" />
-      <ProductDetail productDetail={productDetail} />
-      {/* <ProviderDetail providerDetail={providerDetail} /> */}
-      <ProductReviews productDetail={productDetail} id="reseñar"/>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <ProductDetail productDetail={productDetail} />
+          {/* <ProviderDetail providerDetail={providerDetail} /> */}
+          <ProductReviews productDetail={productDetail} id="reseñar" />
+        </div>
+      )}
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dns from 'dns'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dns from "dns";
+import dotenv from "dotenv";
+import envPlugin from "vite-plugin-env";
 
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), envPlugin],
   server: {
-    host: 'localhost',
+    host: "localhost",
     port: 3001,
-  }
-})
+  },
+});
