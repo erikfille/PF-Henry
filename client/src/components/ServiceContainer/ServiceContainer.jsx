d56e6fa9759d08191a675c3daa5a7dec06b85327
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-30 17:17:28 -0300

    Add loader a service

--- client/src/components/ServiceContainer/ServiceContainer.jsx
+++ client/src/components/ServiceContainer/ServiceContainer.jsx
@@ -1,6 +1,8 @@
 import { useServices } from "../../hooks/useStore";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import servicesImage from '../../../public/images/service-img.jpg'
+import Loader from '../Loader/Loader'
+
 const ServiceContainer = () => {
 	const [filteredServices, allServices] = useServices((state) => [
 		state.filteredServices,
@@ -9,6 +11,58 @@ const ServiceContainer = () => {
 
 	return (
 		<>
+    {allServices.length ? (filteredServices.length && typeof filteredServices === 'object' ? (
+				filteredServices.map((service) => {
+					return (
+						<ServiceCard
+							key={service._id}
+							nombre={service.nombre}
+							tipo={service.tipo}
+							pais={service.pais}
+							direccion={service.direccion}
+							image={servicesImage}
+							mail={service.mail}
+							telefono={service.telefono}
+							descripcion={service.descripcion}
+							rating={service.rating}
+							horarioAtencion={service.horarioAtencion}
+						/>
+					);
+				})
+			) : (
+				<p>Lo sentimos, no hay servicios con esas características</p>
+			)) : <Loader /> }
+
+		</>
+	);
+};
+
+export default ServiceContainer;
+
+/*
+{allServices.lenght ? ({filteredServices.length && typeof filteredServices === 'object' ? (
+				filteredServices.map((service) => {
+					return (
+						<ServiceCard
+							key={service._id}
+							nombre={service.nombre}
+							tipo={service.tipo}
+							pais={service.pais}
+							direccion={service.direccion}
+							image={servicesImage}
+							mail={service.mail}
+							telefono={service.telefono}
+							descripcion={service.descripcion}
+							rating={service.rating}
+							horarioAtencion={service.horarioAtencion}
+						/>
+					);
+				})
+			) : (
+				<p>Lo sentimos, no hay servicios con esas características</p>
+			)}) : <Loader /> }
+
+{allServices.length}
 			{filteredServices.length && typeof filteredServices === 'object' ? (
 				filteredServices.map((service) => {
 					return (
@@ -30,8 +84,5 @@ const ServiceContainer = () => {
 			) : (
 				<p>Lo sentimos, no hay servicios con esas características</p>
 			)}
-		</>
-	);
-};
-
-export default ServiceContainer;
+*/
+{}
