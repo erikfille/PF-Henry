265de607bd538cbdd4f6da5727093f0f4c17c801
Maximiliano Herr <max.herr.88@gmail.com>
2023-03-31 13:01:08 -0300

    Agregado boton fin compra, totalPrice al store

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -5,113 +5,121 @@ import { useState, useEffect } from "react";
 import { useProduct } from "../../hooks/useStore";
 import { NavLink } from "react-router-dom";
 
-
 export default function Cart() {
-  const [isOpen, setIsOpen] = useState(false); // El boton de CART del header debe modificar este estado. inicalmente debe estar en False.
-  const [total, setTotal] = useState(0);
+	const [isOpen, setIsOpen] = useState(false); // El boton de CART del header debe modificar este estado. inicalmente debe estar en False.
 
-  const [cartProducts, cartState, setActiveCart, setCartRemove] = useProduct(
-    (state) => [
-      state.cartProducts,
-      state.cartState,
-      state.setActiveCart,
-      state.setCartRemove,
-    ]
-  );
+	const [
+		cartProducts,
+		cartState,
+		setActiveCart,
+		setCartRemove,
+		setTotalPrice,
+		totalPrice,
+	] = useProduct((state) => [
+		state.cartProducts,
+		state.cartState,
+		state.setActiveCart,
+		state.setCartRemove,
+		state.setTotalPrice,
+		state.totalPrice,
+	]);
 
-  useEffect(() => {
-    setIsOpen(cartState);
-  }, [cartState]);
+	useEffect(() => {
+		setIsOpen(cartState);
+	}, [cartState]);
 
-  useEffect(() => {
-    if (typeof cartProducts === "object" && cartProducts.length) {
-      let totalBuy = 0;
-      cartProducts.forEach((p) => (totalBuy += p.precio * p.quantity));
-      setTotal(totalBuy.toFixed(2));
-    }
-  }, [cartProducts]);
+	useEffect(() => {
+		if (typeof cartProducts === "object" && cartProducts.length) {
+			let totalBuy = 0;
+			cartProducts.forEach((p) => (totalBuy += p.precio * p.quantity));
+			setTotalPrice(totalBuy.toFixed(2));
+		} else {
+			setTotalPrice(0);
+		}
+	}, [cartProducts]);
 
-  return (
-    <>
-      <div
-        className={style.shoppingCart}
-        style={{ display: isOpen ? "block" : "none" }}
-      >
-        <div className="d-flex justify-content-end">
-          <button onClick={() => setActiveCart()} className="button">
-            Cerrar
-          </button>
-        </div>
+	return (
+		<>
+			<div
+				className={style.shoppingCart}
+				style={{ display: isOpen ? "block" : "none" }}>
+				<div className="d-flex justify-content-end">
+					<button onClick={() => setActiveCart()} className="button">
+						Cerrar
+					</button>
+				</div>
 
-        <div
-          className={`${style.titleCart} d-flex flex-column align-items-center my-5 gap-2`}
-        >
-          <img
-            src="images/logo-pet.png"
-            alt="logo-pet"
-            style={{ width: "30px", height: "30px" }}
-          />
-          <h2>Tu Carrito</h2>
-        </div>
+				<div
+					className={`${style.titleCart} d-flex flex-column align-items-center my-5 gap-2`}>
+					<img
+						src="images/logo-pet.png"
+						alt="logo-pet"
+						style={{ width: "30px", height: "30px" }}
+					/>
+					<h2>Tu Carrito</h2>
+				</div>
 
-        <div className={style.imgBg}>
-          <img
-            src="images/logo-bg-product.png"
-            alt="img-logo"
-            style={{ width: "430px" }}
-          />
-        </div>
+				<div className={style.imgBg}>
+					<img
+						src="images/logo-bg-product.png"
+						alt="img-logo"
+						style={{ width: "430px" }}
+					/>
+				</div>
 
-        {!cartProducts.length ? (
-          <>
-            <div
-              className={`${style.cartEmpty} d-flex flex-column align-items-center py-5`}
-            >
-              <p>Tu carrito esta vacio</p>
-              <NavLink onClick={() => setActiveCart()} to="/tienda">Ir a la tienda</NavLink>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={`${style.products} d-flex flex-column align-items-center`}>
-              {typeof cartProducts === "object" &&
-                cartProducts.length &&
-                cartProducts.map(
-                  (
-                    product // Aca se mapea los items de cart del store.
-                  ) => (
-                    <ProductCard // y por cada item renderiza una card con el estilo para el carrito.
-                      key={product._id}
-                      id={product._id}
-                      titulo={product.titulo}
-                      price={product.precio}
-                      imagen={product.imagen}
-                      cant={product.quantity}
-                      showAs="cart"
-                    />
-                  )
-                )}
-            </div>
-            <div className={style.container}>
-              <div className={`${style.totalContainer} mb-5`}>
-                <div
-                  className={`${style.total} d-flex justify-content-between mb-2`}
-                >
-                  <h6>Total ($ USD)</h6>
-                  <h6>{total}</h6>
-                </div>
-              </div>
-              <div className="d-flex justify-content-center">
-                <button className="button" onClick={()=> setActiveCart(false)}>
-                  <NavLink to='/checkout'>
-                    Confirmar pedido
-                  </NavLink>
-                  </button>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-    </>
-  );
+				{!cartProducts.length ? (
+					<>
+						<div
+							className={`${style.cartEmpty} d-flex flex-column align-items-center py-5`}>
+							<p>Tu carrito esta vacio</p>
+							<NavLink onClick={() => setActiveCart()} to="/tienda">
+								Ir a la tienda
+							</NavLink>
+						</div>
+					</>
+				) : (
+					<>
+						<div
+							className={`${style.products} d-flex flex-column align-items-center`}>
+							{typeof cartProducts === "object" &&
+								cartProducts.length &&
+								cartProducts.map(
+									(
+										product // Aca se mapea los items de cart del store.
+									) => (
+										<ProductCard // y por cada item renderiza una card con el estilo para el carrito.
+											key={product._id}
+											id={product._id}
+											titulo={product.titulo}
+											price={product.precio}
+											imagen={product.imagen}
+											cant={product.quantity}
+											showAs="cart"
+										/>
+									)
+								)}
+						</div>
+						<div className={style.container}>
+							<div className={`${style.totalContainer} mb-5`}>
+								<div
+									className={`${style.total} d-flex justify-content-between mb-2`}>
+									<h6>Total ($ USD)</h6>
+									<h6>{totalPrice}</h6>
+								</div>
+							</div>
+							<div className="d-flex justify-content-center">
+								<NavLink to="/checkout">
+									<button
+										className="button"
+										onClick={() => setActiveCart(false)}>
+										Confirmar pedido
+									</button>
+								</NavLink>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
+		</>
+	);
 }
