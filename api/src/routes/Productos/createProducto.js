13b767e1ddb5c60f9ef8fca1955744e4be79bda3
Blesial <chonaquirelli@gmail.com>
2023-04-03 16:25:46 -0300

    Autorizaciones desde el Back segun rol, ruta createProduct ahora necesita token valido

--- api/src/routes/Productos/createProducto.js
+++ api/src/routes/Productos/createProducto.js
@@ -5,50 +5,57 @@ const createProductosRoutes = [
   {
     method: "POST",
     path: "/crearProducto",
-    handler: async (request, h) => {
-      try {
-        const {
-          titulo,
-          animal,
-          tipo,
-          precio,
-          imagen,
-          descripcion,
-          rating,
-          stock,
-          activo,
-          categoria,
-          proveedor,
-          comentario
-        } = request.payload;
-
-        // Buscar la categoría por su nombre
-        const categoriaEncontrada = await Categoria.findOne({ tipo: tipo, nombre: categoria });
-
-        if (!categoriaEncontrada) {
-          return h.response(`La categoría ${categoria} no existe`).code(400);
+    options: {
+      auth: 'jwt', // Agrega autenticación jwt
+      handler: async (request, h) => {
+        try {
+          const {
+            titulo,
+            animal,
+            tipo,
+            precio,
+            imagen,
+            descripcion,
+            rating,
+            stock,
+            activo,
+            categoria,
+            proveedor,
+            comentario
+          } = request.payload;
+  
+          // Buscar la categoría por su nombre
+          const categoriaEncontrada = await Categoria.findOne({ tipo: tipo, nombre: categoria });
+  
+          if (!categoriaEncontrada) {
+            return h.response(`La categoría ${categoria} no existe`).code(400);
+          }
+  
+          const nuevoProductoServicio = new ProductoServicio({
+            titulo,
+            animal,
+            tipo,
+            precio,
+            imagen,
+            descripcion,
+            rating,
+            stock,
+            activo,
+            categoria: categoriaEncontrada._id,
+            proveedor,
+            comentario
+          });
+  
+          const productoServicioGuardado = await nuevoProductoServicio.save();
+          return h.response(productoServicioGuardado).code(201);
+        } catch (error) {
+          return h.response(error.message).code(500);
         }
-
-        const nuevoProductoServicio = new ProductoServicio({
-          titulo,
-          animal,
-          tipo,
-          precio,
-          imagen,
-          descripcion,
-          rating,
-          stock,
-          activo,
-          categoria: categoriaEncontrada._id,
-          proveedor,
-          comentario
-        });
-
-        const productoServicioGuardado = await nuevoProductoServicio.save();
-        return h.response(productoServicioGuardado).code(201);
-      } catch (error) {
-        return h.response(error.message).code(500);
-      }
+      },
+      payload: {
+        allow: ["application/json"],
+        parse: true,
+      },
     },
   },
 ];
