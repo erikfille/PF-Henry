4feb083cc318cf9a9912f753f7684a2000aadb80
Maximiliano Herr <max.herr.88@gmail.com>
2023-04-07 18:56:47 -0300

    Fix login/signUp img

--- client/src/Views/Login/Login.jsx
+++ client/src/Views/Login/Login.jsx
@@ -8,6 +8,7 @@ import { NavLink } from "react-router-dom";
 import styles from "./Login.module.css";
 
 export default function Login() {
+<<<<<<< HEAD
   const location = useLocation();
 
   const actualLocation = location.pathname;
@@ -60,4 +61,50 @@ export default function Login() {
       </div>
     </div>
   );
+=======
+	const location = useLocation();
+
+	const actualLocation = location.pathname;
+
+	let childProps = {};
+
+	if (location.pathname === "/login") {
+		childProps = {
+			type: "login",
+			subtitle: "Ingrese sus datos para acceder",
+			button: "Ingresar",
+			message: "¿No tienes cuenta?",
+			accountAnchor: "¡Crea una cuenta ahora mismo!",
+			anchorPath: "/signup",
+		};
+	}
+
+	if (location.pathname === "/signup") {
+		childProps = {
+			type: "signup",
+			subtitle: "Ingrese sus datos para registrarse",
+			button: "Registrate",
+			message: "¿Ya tienes cuenta?",
+			accountAnchor: "Ir al Login",
+			anchorPath: "/login",
+		};
+	}
+
+	return (
+		<div className={styles.loginContainer}>
+			<div className="row">
+				<div
+					className={`col-12 col-md-6 col-xl-7 d-flex justify-content-center align-items-center ${styles.formContainer}`}>
+					<h1 className="fw-bold position-absolute top-0 start-0 m-4">PetsAmerica</h1>
+					<LoginWidget childProps={childProps} />
+				</div>
+				<div
+					className={`col-6 col-md-6 col-xl-5 d-none d-md-flex position-relative ${styles.imgContainer}`}
+					style={{
+						backgroundImage: `url(${actualLocation === "/login" ? loginDog : signUpCat})`,
+					}}></div>
+			</div>
+		</div>
+	);
+>>>>>>> a543601 (Fix login/signUp img)
 }
