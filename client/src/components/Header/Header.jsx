33287be40a9d1e39853ec2edf94d3161ff233864
Maximiliano Herr <max.herr.88@gmail.com>
2023-04-11 07:16:10 -0300

    Add total count notifications in cart

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,146 +12,135 @@ import styles from "./Header.module.css";
 import DarkMode from "../DarkMode/DarkMode";
 
 const Header = () => {
-  const [userLogged, setUserLogged] = useState(false); // estado hardcodeado solo para trabajar lo que muestra el boton de User.
-  const [setActiveCart] = useProduct((state) => [state.setActiveCart]);
-  const [logoutUser] = useLogin((state) => [state.logoutUser]);
-  const [user, setUser] = useState({});
+	const [userLogged, setUserLogged] = useState(false); // estado hardcodeado solo para trabajar lo que muestra el boton de User.
+	const [setActiveCart, cartProducts] = useProduct((state) => [
+		state.setActiveCart,
+		state.cartProducts,
+	]);
+	const [logoutUser] = useLogin((state) => [state.logoutUser]);
+	const [user, setUser] = useState({});
 
-  useEffect(() => {
-    const localUser = JSON.parse(localStorage.getItem("user"));
-    if (localUser && !user.id) {
-      setUserLogged(true);
-      setUser(localUser);
-    }
-  }, [userLogged]);
+	useEffect(() => {
+		const localUser = JSON.parse(localStorage.getItem("user"));
+		if (localUser && !user.id) {
+			setUserLogged(true);
+			setUser(localUser);
+		}
+	}, [userLogged]);
 
-  console.log(user);
+	console.log(user);
 
-  return (
-    <nav className="navbar navbar-expand-md bg-body-tertiary">
-      <div className="container-fluid">
-        <div
-          className={`d-flex align-items-center ms-3 gap-3 ${styles.logoContainer}`}
-        >
-          <NavLink to="/" className={styles.navlink}>
-            PetsAmerica
-          </NavLink>
-          <img
-            src={Logo}
-            alt="Logo"
-            className={`align-text-top ms-2 ${styles.logo}`}
-          />
-        </div>
-        <BiMenu
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          className={styles.menuButton}
-        />
-        <div
-          className={`${styles.navContainer} collapse navbar-collapse justify-content-end me-4`}
-          id="navbarNav"
-        >
-          <div
-            className="navbar-nav align-items-center"
-            style={{
-              backgroundColor: "var(--body_background)",
-              width: "100vw !important",
-            }}
-          >
-            <NavLink to="/tienda" className="nav-item me-3">
-              Tienda
-            </NavLink>
-            <NavLink to="/servicios" className="nav-item me-3">
-              Servicios
-            </NavLink>
-            <div
-              className={`d-flex flex-column flex-md-row align-items-center justify-content-center ${styles.buttonContainer}`}
-            >
-              <div className="dropdown">
-                <span
-                  className={`${styles.buttonCart} dropdown-toggle`}
-                  role="button"
-                  data-bs-toggle="dropdown"
-                >
-                  {userLogged ? (
-                    <img
-                      src={user.image}
-                      alt="user-pic"
-                      className={`rounded-circle me-1 ${styles.imgProfile}`}
-                    />
-                  ) : (
-                    <HiOutlineUserCircle className={styles.buttonUser} />
-                  )}
-                </span>
-                {!userLogged ? (
-                  <>
-                    <ul
-                      className={`${styles.bgColor} dropdown-menu dropdown-menu-end`}
-                      aria-labelledby="dropdownMenuLink"
-                    >
-                      <li>
-                        <Link
-                          to="/login"
-                          className={`${styles.li} dropdown-item`}
-                        >
-                          Iniciar Sesión
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/signup"
-                          className={`${styles.li} dropdown-item`}
-                        >
-                          Registrarte
-                        </Link>
-                      </li>
-                    </ul>
-                  </>
-                ) : (
-                  <>
-                    <ul
-                      className={`${styles.dropDownMenu} dropdown-menu dropdown-menu-end`}
-                      aria-labelledby="dropdownMenuLink"
-                    >
-                      <li>
-                        <Link
-                          to={`/perfil/${user.id}`}
-                          className={`${styles.li} dropdown-item`}
-                        >
-                          Ver perfil
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to=""
-                          onClick={() => logoutUser()}
-                          className={`${styles.li} dropdown-item`}
-                        >
-                          Cerrar sesión
-                        </Link>
-                      </li>
-                    </ul>
-                  </>
-                )}
-              </div>
-              <NavLink
-                className="nav-item me-3"
-                id="cartButton"
-                onClick={() => setActiveCart()}
-              >
-                <MdOutlineShoppingCart className={styles.iconCart} />
-              </NavLink>
-              <DarkMode />
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navbar navbar-expand-md bg-body-tertiary">
+			<div className="container-fluid">
+				<div className={`d-flex align-items-center ms-3 gap-3 ${styles.logoContainer}`}>
+					<NavLink to="/" className={styles.navlink}>
+						PetsAmerica
+					</NavLink>
+					<img src={Logo} alt="Logo" className={`align-text-top ms-2 ${styles.logo}`} />
+				</div>
+				<BiMenu
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+					className={styles.menuButton}
+				/>
+				<div
+					className={`${styles.navContainer} collapse navbar-collapse justify-content-end me-4`}
+					id="navbarNav">
+					<div
+						className="navbar-nav align-items-center"
+						style={{
+							backgroundColor: "var(--body_background)",
+							width: "100vw !important",
+						}}>
+						<NavLink to="/tienda" className="nav-item me-3">
+							Tienda
+						</NavLink>
+						<NavLink to="/servicios" className="nav-item me-3">
+							Servicios
+						</NavLink>
+						<div
+							className={`d-flex flex-column flex-md-row align-items-center justify-content-center ${styles.buttonContainer}`}>
+							<div className="dropdown">
+								<span
+									className={`${styles.buttonCart} dropdown-toggle`}
+									role="button"
+									data-bs-toggle="dropdown">
+									{userLogged ? (
+										<img
+											src={user.image}
+											alt="user-pic"
+											className={`rounded-circle me-1 ${styles.imgProfile}`}
+										/>
+									) : (
+										<HiOutlineUserCircle className={styles.buttonUser} />
+									)}
+								</span>
+								{!userLogged ? (
+									<>
+										<ul
+											className={`${styles.bgColor} dropdown-menu dropdown-menu-end`}
+											aria-labelledby="dropdownMenuLink">
+											<li>
+												<Link to="/login" className={`${styles.li} dropdown-item`}>
+													Iniciar Sesión
+												</Link>
+											</li>
+											<li>
+												<Link to="/signup" className={`${styles.li} dropdown-item`}>
+													Registrarte
+												</Link>
+											</li>
+										</ul>
+									</>
+								) : (
+									<>
+										<ul
+											className={`${styles.dropDownMenu} dropdown-menu dropdown-menu-end`}
+											aria-labelledby="dropdownMenuLink">
+											<li>
+												<Link
+													to={`/perfil/${user.id}`}
+													className={`${styles.li} dropdown-item`}>
+													Ver perfil
+												</Link>
+											</li>
+											<li>
+												<Link
+													to=""
+													onClick={() => logoutUser()}
+													className={`${styles.li} dropdown-item`}>
+													Cerrar sesión
+												</Link>
+											</li>
+										</ul>
+									</>
+								)}
+							</div>
+							<NavLink
+								className="nav-item me-3"
+								id="cartButton"
+								onClick={() => setActiveCart()}>
+								<div className="position-relative">
+									<MdOutlineShoppingCart className={styles.iconCart} />
+									{cartProducts.length ? (
+										<div className={styles.totalCart}>{cartProducts.length}</div>
+									) : (
+										""
+									)}
+								</div>
+							</NavLink>
+							<DarkMode />
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
