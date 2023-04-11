684ce6072b540f1a87acaf856b2d682121b22f8f
Noelia Lombardo <noedenisel@MacBook-Pro-de-Noe-Denise.local>
2023-04-11 16:32:53 -0300

    cambios menores

--- client/src/Views/Checkout/PayPal.jsx
+++ client/src/Views/Checkout/PayPal.jsx
@@ -1,34 +1,17 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useModal, useProduct } from "../../hooks/useStore";
 
-import axios from "axios"
+
 
 export default function PayPal(props) {
   const [setModalInfo] = useModal((state) => [state.setModalInfo]);
-
-
   const [deleteCartContent] = useProduct((state) => [state.deleteCartContent]);
-
-  async function onApproveAction(data) {
-    await axios
-      .post("/paypal-transaction-complete", { orderID: data.orderID })
-      console.log(data.orderId)
-      .then(() => {
-        console.log(data);
-        deleteCartContent()
-        window.location.assign("/");
-        
-      });
-      
-  } 
-
-
-  
+ 
   return (
+
     <PayPalScriptProvider
       options={{
-        "client-id":
-          "AeL7yM8K1K-s4nfOXeKfU3rt4QbKBpiWrqiwqcdwZStk-UOhIDb9qEmBhUSBjTkVn5AmyF2E60PnHzXc",
+        "client-id": "AeL7yM8K1K-s4nfOXeKfU3rt4QbKBpiWrqiwqcdwZStk-UOhIDb9qEmBhUSBjTkVn5AmyF2E60PnHzXc",
         currency: "USD",
       }}
     >
@@ -46,28 +29,28 @@ export default function PayPal(props) {
         }}
         onApprove={(data, actions) => {
           return actions.order.capture().then(function (details) {
-            setModalInfo(
-            setModalInfo(
-              "Compra Exitosa",
-              `${details.payer.name.given_name} tu compra se realizó con éxito, en breve serás redirijido a tu panel de usuario`,
-              onApproveAction(),
-              [data]
-            );
-            ;
+              setModalInfo(
+                  "Compra Exitosa",
+                   details.payer.name.given_name + "Tu compra se realizó con éxito, en breve serás redirigido a tu panel de usuario"
+                );
+              deleteCartContent()
+
             // OPTIONAL: Call your server to save the transaction
-            // return fetch("/paypal-transaction-complete", {
-            //   method: "post",
-            //   body: JSON.stringify({
-            //     orderID: data.orderID,
-            //   }),
-            // }).then(() => {
-            //   setTimeout(() => {
-            //     window.location.href = "/"; // Redirigir a la página de inicio
-            //   }, 10000); // Mostrar la alerta durante 3 segundos
-            // });
+            return fetch("/paypal-transaction-complete", {
+              method: "post",
+              body: JSON.stringify({
+                orderID: data.orderID
+              })
+            }).then(() => {
+              setTimeout(() => {
+                window.location.href = "/"; // Redirigir a la página de inicio
+              }, 3000); // Mostrar la alerta durante 3 segundos
+            });
           });
         }}
+
       />
     </PayPalScriptProvider>
-  );
+
+);
 }
