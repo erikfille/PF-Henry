e0498d383de55ab8cfb1e552b662fea8a8a7e313
Maximiliano Herr <max.herr.88@gmail.com>
2023-04-12 08:06:49 -0300

    Fix loader bad import

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,13 +1,14 @@
 import style from "./Loader.module.css";
+import logo from "../../images/logo-pet.png";
 
 const Loader = () => {
-  return (
-    <>
-      <div className={style.loader}>
-        <img src="src/images/logo-pet.png" alt="Dog Paw Icon" />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={style.loader}>
+				<img src={logo} alt="Dog Paw Icon" />
+			</div>
+		</>
+	);
 };
 
 export default Loader;
