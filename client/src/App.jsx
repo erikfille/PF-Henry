2e377e6cb79831cc71768e15e22d10ff05f26fbc
erikfille <109306990+erikfille@users.noreply.github.com>
2023-04-13 13:38:25 -0300

    Deploy Data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,14 +43,7 @@ import DashProduct from "./Views/DashboardAdmin/DashProduct/DashProduct";
 import DashCategories from "./Views/DashboardAdmin/DashCategories/DashCategories";
 import DashAnimals from "./Views/DashboardAdmin/DashAnimals/DashAnimals";
 
-import AdminDashboard from "./Views/DashboardAdmin/AdminDashboard";
-import DashUser from "./Views/DashboardAdmin/DashUser/DashUser";
-import DashProvider from "./Views/DashboardAdmin/DashProvider/DashProvider";
-import DashProduct from "./Views/DashboardAdmin/DashProduct/DashProduct";
-import DashCategories from "./Views/DashboardAdmin/DashCategories/DashCategories";
-import DashAnimals from "./Views/DashboardAdmin/DashAnimals/DashAnimals";
-
-axios.defaults.baseURL = "pf-henry-production.up.railway.app";
+axios.defaults.baseURL = "https://pf-henry-production.up.railway.app";
 
 function App() {
   const location = useLocation();
