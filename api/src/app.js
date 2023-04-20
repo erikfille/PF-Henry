f6c666eaf11278926d385a277392b4a047de5f40
erikfille <109306990+erikfille@users.noreply.github.com>
2023-04-20 19:48:18 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -45,7 +45,7 @@ const init = async () => {
     plugin: HapiCors,
     options: {
       methods: ["PUT", "POST", "DELETE", "GET"],
-      origins: ["https://pf-henry-production.up.railway.app"],
+      origins: ["*"],
     },
   });
 
