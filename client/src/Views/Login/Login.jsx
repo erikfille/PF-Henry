a9ed81739000169a68bd72232f91e7be1a4d1eb8
lllMaxMaxlll <max.herr.88@gmail.com>
2023-04-21 11:49:28 -0300

    Fix meta signUp & login

--- client/src/Views/Login/Login.jsx
+++ client/src/Views/Login/Login.jsx
@@ -5,6 +5,7 @@ import loginDog from "../../assets/img/login/login-dog.jpeg";
 import signUpCat from "../../assets/img/login/signUp-cat.jpeg";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.png";
+import Meta from "../../components/Meta/Meta";
 
 import styles from "./Login.module.css";
 
@@ -38,22 +39,25 @@ export default function Login() {
 	}
 
 	return (
-		<div className={styles.loginContainer}>
-			<div className="row">
-				<div
-					className={`col-12 col-md-6 col-xl-7 d-flex justify-content-center align-items-center ${styles.formContainer} mt-4`}
-          style={{height: location.pathname === "/signup" ? '100%' : '100vh'}}>
-					<NavLink to="/">
-						<h1 className="fw-bold position-absolute top-0 start-0 m-4">PetsAmerica</h1>
-					</NavLink>
-					<LoginWidget childProps={childProps} />
-				</div>
-				<div
-					className={`col-6 col-md-6 col-xl-5 d-none d-md-flex position-relative ${styles.imgContainer}`}
-					style={{
-						backgroundImage: `url(${actualLocation === "/login" ? loginDog : signUpCat})`,
-					}}></div>
-			</div>
-		</div>
+    <>
+      <Meta title={location.pathname === "/login" ? "Login" : "SignUp"} />
+      <div className={styles.loginContainer}>
+		    <div className="row">
+		  		<div
+		  			className={`col-12 col-md-6 col-xl-7 d-flex justify-content-center align-items-center ${styles.formContainer} mt-4`}
+            style={{height: location.pathname === "/signup" ? '100%' : '100vh'}}>
+		  			<NavLink to="/">
+		  				<h1 className="fw-bold position-absolute top-0 start-0 m-4">PetsAmerica</h1>
+		  			</NavLink>
+		  			<LoginWidget childProps={childProps} />
+		  		</div>
+		  		<div
+		  			className={`col-6 col-md-6 col-xl-5 d-none d-md-flex position-relative ${styles.imgContainer}`}
+		  			style={{
+              backgroundImage: `url(${actualLocation === "/login" ? loginDog : signUpCat})`,
+		  			}}></div>
+		  	</div>
+		  </div>
+    </>
 	);
 }
