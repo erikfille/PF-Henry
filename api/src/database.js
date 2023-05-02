f84287b6633052d4df8679bc573bc6e15a7ff3d4
erikfille <109306990+erikfille@users.noreply.github.com>
2023-05-02 20:00:06 -0300

    Deploy Config

--- api/src/database.js
+++ api/src/database.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
 // Production
-// mongoose
-//   .connect(
-//     "mongodb://mongo:Zj4zIGBVGZqdee6mK3WO@containers-us-west-204.railway.app:7641",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => console.log("la base de datos esta conectada"))
-//   .catch((err) => console.log(err));
-
-// Development
 mongoose
-  .connect("mongodb+srv://PetCare:HenryVip@petcare.krjtdtk.mongodb.net/test", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb://mongo:Zj4zIGBVGZqdee6mK3WO@containers-us-west-204.railway.app:7641",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("la base de datos esta conectada"))
   .catch((err) => console.log(err));
+
+// Development
+// mongoose
+//   .connect("mongodb+srv://PetCare:HenryVip@petcare.krjtdtk.mongodb.net/test", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("la base de datos esta conectada"))
+//   .catch((err) => console.log(err));
