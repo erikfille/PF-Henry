0b5566431e8984a4a00e3aea291c9994c31e97fd
erikfille <109306990+erikfille@users.noreply.github.com>
2023-05-02 19:56:26 -0300

    Production config

--- api/src/database.js
+++ api/src/database.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://mongo:Zj4zIGBVGZqdee6mK3WO@containers-us-west-204.railway.app:7641", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
-    
 })
 .then(() => console.log("la base de datos esta conectada"))
     .catch(err => console.log(err))
+    
+
+// mongoose.connect("mongodb://mongo:Zj4zIGBVGZqdee6mK3WO@containers-us-west-204.railway.app:7641", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then(() => console.log("la base de datos esta conectada"))
+//     .catch(err => console.log(err))
     
